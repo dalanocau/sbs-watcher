663ff1f55a55329112a235f3a6b5c6c8ea6569e2
dalanocau <dalanocau@unsa.edu.pe>
2026-06-01 17:36:09 -0500

    v55.1

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def ciclo_verificacion():
             check_website_changes()
         except Exception as e:
             print(f"❌ Error en verificación: {e}")
-        time.sleep(30)  # cada 30 minutos
+        time.sleep(5)  # cada 30 minutos
 
 # ------------------- SERVIDOR WEB -------------------
 app = Flask(__name__)
